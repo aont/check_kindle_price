f55458faf6c5e34dcb15a8a8ffc3f63065e6dc04
aont <>
2019-10-09 10:14:00 +0900

    price match modified, u+quote->quote

--- main.py
+++ main.py
@@ -20,20 +20,20 @@ import cssselect
 import sendgrid
 
 sleep_duration = 5
-AMAZON_CO_JP=u'https://www.amazon.co.jp/'
+AMAZON_CO_JP='https://www.amazon.co.jp/'
 amazon_headers = {
-    u'authority': u'www.amazon.co.jp',
-    u'upgrade-insecure-requests': u'1',
-    u'user-agent': u'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
-    u'dnt': u'1',
-    u'accept': u'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-    u'accept-encoding': u'gzip, deflate, br',
-    u'accept-language': u'ja-JP,ja;q=0.9,en-US;q=0.8,en;q=0.7',
-    u'referer': AMAZON_CO_JP,
+    'authority': 'www.amazon.co.jp',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+    'dnt': '1',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'ja-JP,ja;q=0.9,en-US;q=0.8,en;q=0.7',
+    'referer': AMAZON_CO_JP,
 }
 
 def send_mail(message_str, subject):
-    sys.stderr.write(u"[info] mailing via sendgrid\n")
+    sys.stderr.write("[info] mailing via sendgrid\n")
     sg_username = os.environ["SENDGRID_USERNAME"]
     sg_recipient = os.environ["SENDGRID_RECIPIENT"]
     sg_apikey = os.environ["SENDGRID_APIKEY"]
@@ -52,31 +52,31 @@ def str_abbreviate(str_in):
 
 def pg_execute(pg_cur, query, param=None):
     param_str = str_abbreviate("%s" % param)
-    sys.stderr.write(u'[info] postgres: %s param=%s\n' % (query, param_str))
+    sys.stderr.write('[info] postgres: %s param=%s\n' % (query, param_str))
     return pg_cur.execute(query, param)
 
 def pg_init_json(pg_cur, table_name, key_name):
-    pg_result = pg_execute(pg_cur, u"select 1 from pg_tables where schemaname='public' and tablename=%s ;", [table_name])
+    pg_result = pg_execute(pg_cur, "select 1 from pg_tables where schemaname='public' and tablename=%s ;", [table_name])
     pg_result = pg_cur.fetchone()
     if pg_result is None:
-        #sys.stderr.write(u'[info] creating table\n')
-        pg_execute(pg_cur, u"create table %s (key text unique, value text);" % (table_name))
+        #sys.stderr.write('[info] creating table\n')
+        pg_execute(pg_cur, "create table %s (key text unique, value text);" % (table_name))
     elif 1 != pg_result[0] :
-        raise Exception(u"exception")
+        raise Exception("exception")
 
-    pg_execute(pg_cur, u'select value from %s where key=%%s;' % table_name, [key_name])
+    pg_execute(pg_cur, 'select value from %s where key=%%s;' % table_name, [key_name])
     pg_result = pg_cur.fetchone()
     
     if pg_result is None:
-        pg_execute(pg_cur, u'insert into %s VALUES (%%s, %%s);' % table_name, [key_name, u"{}"])
+        pg_execute(pg_cur, 'insert into %s VALUES (%%s, %%s);' % table_name, [key_name, "{}"])
         pg_data = {}
     else:
-        sys.stderr.write(u'[info] data=%s\n' % str_abbreviate(pg_result[0]))
+        sys.stderr.write('[info] data=%s\n' % str_abbreviate(pg_result[0]))
         pg_data = json.loads(pg_result[0])
     return pg_data
 
 def pg_update_json(pg_cur, table_name, key_name, pg_data):
-    return pg_execute(pg_cur, u'update %s set value = %%s where key = %%s;' % table_name, [json.dumps(pg_data, ensure_ascii=False), key_name])
+    return pg_execute(pg_cur, 'update %s set value = %%s where key = %%s;' % table_name, [json.dumps(pg_data, ensure_ascii=False), key_name])
 
 def get_wish_list(sess, list_id):
     item_ary = []
@@ -86,14 +86,14 @@ def get_wish_list(sess, list_id):
         if lastEvaluatedKey is None:
             break
         else:
-            sys.stderr.write(u"[info] lastEvaluatedKey: %s\n" % lastEvaluatedKey)
+            sys.stderr.write("[info] lastEvaluatedKey: %s\n" % lastEvaluatedKey)
         
-    sys.stderr.write(u"[info] get_wish_list done\n")
+    sys.stderr.write("[info] get_wish_list done\n")
     return item_ary
 
 def get_wish_list_page(sess, list_id, item_ary, lastEvaluatedKey = None):
 
-    url = AMAZON_CO_JP + u'hz/wishlist/ls/' + list_id
+    url = AMAZON_CO_JP + 'hz/wishlist/ls/' + list_id
     if lastEvaluatedKey is not None:
         url += "?lek=" + lastEvaluatedKey
 
@@ -104,56 +104,56 @@ def get_wish_list_page(sess, list_id, item_ary, lastEvaluatedKey = None):
         if requests.codes.get("ok") == result.status_code:
             break
         else:
-            sys.stderr.write(u"[info] amazon status_code = %s\n" % result.status_code)
-            sys.stderr.write(u"[info] wait for 5s and retry\n")
+            sys.stderr.write("[info] amazon status_code = %s\n" % result.status_code)
+            sys.stderr.write("[info] wait for 5s and retry\n")
             # sys.stderr.flush()
             try_num += 1
             if try_num == max_try:
                 send_mail(result.text, "Alert")
-                raise Exception(u'unexpected')
+                raise Exception('unexpected')
             time.sleep(sleep_duration)
             continue
     
     product_lxml = lxml.html.fromstring(result.text)
     try:
-        g_items = product_lxml.get_element_by_id(u'g-items')
+        g_items = product_lxml.get_element_by_id('g-items')
     except KeyError as e:
         send_mail(result.text, "Alert")
         raise e
-    li_ary = g_items.cssselect(u'li')
+    li_ary = g_items.cssselect('li')
 
     # item_ary = []
     dp_pattern = re.compile('/dp/(.*?)/')
     for li in li_ary:
-        data_itemid = li.get(u"data-itemid")
+        data_itemid = li.get("data-itemid")
         # sys.stderr.write("[Info] data-itemid: %s \n" % data_itemid)
-        itemname_elem = li.get_element_by_id(u'itemName_%s' % data_itemid)
-        item_title = itemname_elem.get(u'title')
-        item_href = itemname_elem.get(u'href')
+        itemname_elem = li.get_element_by_id('itemName_%s' % data_itemid)
+        item_title = itemname_elem.get('title')
+        item_href = itemname_elem.get('href')
         # item_html = itemname_elem.text
         # sys.stdout.write("%s %s %s\n" % (item_title, item_href, item_html))
         dp_match = dp_pattern.search(item_href)
         if dp_match is None:
-            raise Exception(u"unexpected")
+            raise Exception("unexpected")
         # sys.stdout.write("dpid %s\n" % dp_match.group(1))
 
         dp_id = dp_match.group(1)
-        item_ary.append({u"dp": dp_id, u'title': item_title})
+        item_ary.append({"dp": dp_id, 'title': item_title})
     
-    # showMoreUrl = product_lxml.cssselect(u'#g-items > form > input.showMoreUrl')
-    lastEvaluatedKey_elems = product_lxml.cssselect(u'input.lastEvaluatedKey')
+    # showMoreUrl = product_lxml.cssselect('#g-items > form > input.showMoreUrl')
+    lastEvaluatedKey_elems = product_lxml.cssselect('input.lastEvaluatedKey')
     if len(lastEvaluatedKey_elems)==0:
         return None
     elif len(lastEvaluatedKey_elems)==1:
-        return lastEvaluatedKey_elems[0].get(u"value")
+        return lastEvaluatedKey_elems[0].get("value")
     else:
-        raise Exception(u"unexpected")
+        raise Exception("unexpected")
     # return item_ary
 
 
-AMAZON_DP= AMAZON_CO_JP  + u'dp/'
+AMAZON_DP= AMAZON_CO_JP  + 'dp/'
 def check_amazon(sess, dp):
-    sys.stderr.write(u'[info] check_amazon dp=%s\n' % dp)
+    sys.stderr.write('[info] check_amazon dp=%s\n' % dp)
     product_uri = AMAZON_DP + dp
 
     try_num = 0
@@ -163,36 +163,36 @@ def check_amazon(sess, dp):
         result = sess.get(product_uri, headers = amazon_headers)
         # result = sess.get(product_uri)
         if requests.codes.get("unavailable") == result.status_code:
-            sys.stderr.write(u"[info] amazon temporarily unavailable\n")
-            sys.stderr.write(u"[info] wait for 5s and retry\n")
+            sys.stderr.write("[info] amazon temporarily unavailable\n")
+            sys.stderr.write("[info] wait for 5s and retry\n")
             # sys.stderr.flush()
             time.sleep(sleep_duration)
             try_num += 1
             if try_num == max_try:
                 send_mail(result.text, "Alert")
                 # sys.stdout.write(result.text)
-                raise Exception(u'unexpected')
+                raise Exception('unexpected')
             else:
                 continue
 
         if requests.codes.get("ok") != result.status_code:
-            sys.stderr.write(u"[info] amazon status_code = %s\n" % result.status_code)
-            sys.stderr.write(u"[info] wait for 5s and retry\n")
+            sys.stderr.write("[info] amazon status_code = %s\n" % result.status_code)
+            sys.stderr.write("[info] wait for 5s and retry\n")
             # sys.stderr.flush()
             time.sleep(sleep_duration)
             try_num += 1
             if try_num == max_try:
                 send_mail(result.text, "Alert")
                 # sys.stdout.write(result.text)
-                raise Exception(u'unexpected')
+                raise Exception('unexpected')
             else:
                 continue
         
         product_lxml = lxml.html.fromstring(result.text)
-        price_td_ary = product_lxml.cssselect(u'tr.kindle-price> td.a-color-price')
+        price_td_ary = product_lxml.cssselect('tr.kindle-price> td.a-color-price')
 
         if len(price_td_ary) != 1:
-            sys.stderr.write(u"[warn] amazon html format error. retrying...\n")
+            sys.stderr.write("[warn] amazon html format error. retrying...\n")
             time.sleep(sleep_duration)
             # sys.stderr.flush()
             # sys.stdout.write(result.content)
@@ -202,7 +202,7 @@ def check_amazon(sess, dp):
             if try_num == max_try:
                 send_mail(result.text, "Alert")
                 # sys.stdout.write(result.text)
-                raise Exception(u'unexpected')
+                raise Exception('unexpected')
             else:
                 continue
         else:
@@ -210,17 +210,18 @@ def check_amazon(sess, dp):
         
     price_td = price_td_ary[0]
     price_innerhtml = lxml.etree.tostring(price_td).decode()
+    # sys.stderr.write('[info] price_innerhtml=%s\n' % price_innerhtml)
     # print price_innerhtml
-    price_pattern = re.compile(u'&#65509; ([0-9,]+)')
+    price_pattern = re.compile('&#65509;\s*([0-9,]+)')
     price_match_obj = price_pattern.search(price_innerhtml)
     if price_match_obj is not None:
         price_num = int(price_match_obj.group(1).replace(',',''))
         # print "%s yen" % price_num
 
     point_num = 0
-    point_td_ary = product_lxml.cssselect(u'tr.loyalty-points > td.a-align-bottom')
+    point_td_ary = product_lxml.cssselect('tr.loyalty-points > td.a-align-bottom')
     if len(point_td_ary) > 1:
-        raise Exception(u"unexpected")
+        raise Exception("unexpected")
     elif len(point_td_ary) == 1:
         point_td = point_td_ary[0]
         point_innerhtml = lxml.etree.tostring(point_td).decode()
@@ -228,11 +229,11 @@ def check_amazon(sess, dp):
         point_pattern = re.compile('([0-9,]+)pt')
         point_match_obj = point_pattern.search(point_innerhtml)
         if point_match_obj is not None:
-            point_num = int(point_match_obj.group(1).replace(u',',u''))
+            point_num = int(point_match_obj.group(1).replace(',',''))
 
     try:
         # upsell_button_announce = 
-        product_lxml.get_element_by_id(u'upsell-button-announce')
+        product_lxml.get_element_by_id('upsell-button-announce')
         # if upsell_button_announce is not None:
         sys.stderr.write("[Info] unlimited!\n")
         price_num = - price_num
@@ -245,21 +246,21 @@ def check_amazon(sess, dp):
 def main():
     amazon_sess = requests.session()    
 
-    pg_url = os.environ[u'DATABASE_URL']
-    table_name = u'generic_text_data'
-    key_name = u'kindle_price'
+    pg_url = os.environ['DATABASE_URL']
+    table_name = 'generic_text_data'
+    key_name = 'kindle_price'
     pg_conn = psycopg2.connect(pg_url)
     pg_cur = pg_conn.cursor()
     kindle_price_data = pg_init_json(pg_cur, table_name, key_name)
 
-    list_id = os.environ[u'AMAZON_WISH_LIST_ID']
+    list_id = os.environ['AMAZON_WISH_LIST_ID']
     item_ary = get_wish_list(amazon_sess, list_id)
 
     messages = []
     kindle_price_data_new = {}
     for item in item_ary:
-        dp = item[u'dp']
-        item_title = item[u'title']
+        dp = item['dp']
+        item_title = item['title']
 
         if dp not in kindle_price_data:
             prev_net_price = -1
@@ -269,9 +270,9 @@ def main():
         datetime_now = datetime.datetime.now()
         new_state = check_amazon(amazon_sess, dp)
         new_net_price = new_state[0] - new_state[1]
-        sys.stderr.write(u'[info] price=%s point=%s net_price=%s\n' % (new_state[0], new_state[1], new_net_price))
+        sys.stderr.write('[info] price=%s point=%s net_price=%s\n' % (new_state[0], new_state[1], new_net_price))
         if new_net_price != prev_net_price:
-            mes = u"<a href=\"%s%s\">%s</a> %s <- %s" % (AMAZON_DP, dp, item_title, new_net_price, prev_net_price)
+            mes = "<a href=\"%s%s\">%s</a> %s <- %s" % (AMAZON_DP, dp, item_title, new_net_price, prev_net_price)
             messages.append(mes)
             sys.stderr.write("[info] %s\n" %mes)
         
@@ -279,7 +280,7 @@ def main():
             "title": item_title, \
             "price": new_state[0], \
             "point": new_state[1], \
-            "date": datetime_now.strftime(u"%Y/%m/%d %H:%M:%S") \
+            "date": datetime_now.strftime("%Y/%m/%d %H:%M:%S") \
         }
 
     pg_update_json(pg_cur, table_name, key_name, kindle_price_data_new)
@@ -291,5 +292,5 @@ def main():
     pg_conn.commit()
     pg_conn.close()
 
-if __name__ == u'__main__':
+if __name__ == '__main__':
     main()
